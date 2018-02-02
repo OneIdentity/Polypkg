f00975d1b45c52e95b4f8c3668d773bf916d7ef7
JaysonHurst <jayson.hurst@quest.com>
2018-02-02 03:11:33 -0700

    Initial checkin for freebsd support. There is still loads of debug that needs to be cleaned up

--- example/hello.pp
+++ example/hello.pp
@@ -6,24 +6,46 @@
   summary="Hello world"
   description="Engenders a global greeting"
 
+%set [bsd]
+  pp_bsd_message="Greetings!"
+  pp_bsd_category="security"
+  pp_bsd_origin="security/${pp_bsd_name:-$name}"
+  pp_bsd_prefix="/tmp/${pp_bsd_name:-$name}"
+
 %depend
+  grep
+
+%pre
+  echo This is the PRE-INSTALL script
 
 %post
   echo This is the POST-INSTALL script
   %(pp_functions pp_mkuser)
 
+
 %preun
-  echo This is the UNINSTALL script
+  echo This is the PRE-UNINSTALL script
+%postun
+  echo This is the POST-UNINSTALL script
 
 %service hellod
   cmd=${sbindir}/hellod
 
 %files
+  ${bindir}/
+  ${sbindir}/
+  ${sysconfdir}/
+  ${libdir}/
   ${bindir}/hello
   ${bindir}/goodbye
   ${sbindir}/hellod
   ${sysconfdir}/hello.conf  644 volatile
   ${libdir}/hello/
+  ${libdir}/slink
 
 %files dev
+  ${bindir}/
   ${bindir}/hello-config
+
+%fixup
+  echo This is the FIXUP script
